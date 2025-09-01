55e81d3a363d79393bd8e1159dbc87925d191bad
OH016 <OH016k@163.com>
2025-09-02 04:55:51 +0800

    MY Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# 个人dmenu配置
+
+## 使用的补丁
+
+1. dmenu-desktoponly-20240811-475d809.diff
--- dmenu.c
+++ dmenu.c
@@ -745,16 +745,20 @@ main(int argc, char *argv[])
 			mon = atoi(argv[++i]);
 		else if (!strcmp(argv[i], "-p"))   /* adds prompt to left of input field */
 			prompt = argv[++i];
-		else if (!strcmp(argv[i], "-fn"))  /* font or font set */
-			fonts[0] = argv[++i];
-		else if (!strcmp(argv[i], "-nb"))  /* normal background color */
-			colors[SchemeNorm][ColBg] = argv[++i];
-		else if (!strcmp(argv[i], "-nf"))  /* normal foreground color */
-			colors[SchemeNorm][ColFg] = argv[++i];
-		else if (!strcmp(argv[i], "-sb"))  /* selected background color */
-			colors[SchemeSel][ColBg] = argv[++i];
-		else if (!strcmp(argv[i], "-sf"))  /* selected foreground color */
-			colors[SchemeSel][ColFg] = argv[++i];
+
+		// OH016 : 不使用定义字体和颜色功能
+//		else if (!strcmp(argv[i], "-fn"))  // font or font set 
+//			fonts[0] = argv[++i];
+//		else if (!strcmp(argv[i], "-nb"))  // normal background color 
+//			colors[SchemeNorm][ColBg] = argv[++i];
+//		else if (!strcmp(argv[i], "-nf"))  // normal foreground color 
+//			colors[SchemeNorm][ColFg] = argv[++i];
+//		else if (!strcmp(argv[i], "-sb"))  // selected background color 
+//			colors[SchemeSel][ColBg] = argv[++i];
+//		else if (!strcmp(argv[i], "-sf"))  // selected foreground color 
+//			colors[SchemeSel][ColFg] = argv[++i];
+		// OH016 : END
+
 		else if (!strcmp(argv[i], "-w"))   /* embedding window id */
 			embed = argv[++i];
 		else
